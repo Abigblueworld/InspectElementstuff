e52d9f8ce1f40123fb5b8c4d0b4a292f48eb1300
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-04-30 17:28:12 -0400

    Created the plugin (You'll need to insert the HTML code provided soon

--- Antinspect.js
+++ Antinspect.js
@@ -1,2 +1,10 @@
-######Antinspect - stopping bad actions from being made (by Abigblueworld)######
+/////////Antinspect - stopping bad actions from being made (by Abigblueworld)\\\\\\\\
+/////// Check my Github out: https://github.com/Abigblueworld \\\\\\\\\\\
 
+While (true) {
+  Unnamed = Scanner
+    if (not Scanner == Unnamed) {
+      console.log("Insert message")
+      Scanner = Unnamed
+    }
+}
