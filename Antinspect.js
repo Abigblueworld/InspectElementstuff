fb52d4a3421e6f6a611d65adecf6448447ab6abf
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-04-30 17:30:10 -0400

    Here's the HTML :D
    hi

--- Antinspect.js
+++ Antinspect.js
@@ -1,6 +1,8 @@
 /////////Antinspect - stopping bad actions from being made (by Abigblueworld)\\\\\\\\
 /////// Check my Github out: https://github.com/Abigblueworld \\\\\\\\\\\
 
+//Delete this extra when done: <script src="/Antiinspect.js"></script>
+
 While (true) {
   Unnamed = Scanner
     if (not Scanner == Unnamed) {
