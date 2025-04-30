7e7b7af82e1f07565a60f280df81cd75790a9fe0
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-04-30 17:39:29 -0400

    Sitepoint thanks for that epik code for sleep!
    👯

--- Antinspect.js
+++ Antinspect.js
@@ -3,10 +3,19 @@
 
 //Delete this extra when done: <script src="/Antiinspect.js"></script>
 
+////Code from Sitepoint <3
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+////Code by me :3
+
 While (true) {
   Unnamed = Scanner
+  sleep(300).then(()
     if (not Scanner == Unnamed) {
       console.log("Insert message")
-      Scanner = Unnamed
+      Scanner = Unnamed)
     }
 }
